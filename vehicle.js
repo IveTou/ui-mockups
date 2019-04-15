3cbc5350095b6d52c431a3724e8a15764b0ee365
Ive <ive.tourinho@itmov.com.br>
2019-04-15 11:55:46 -0300

    See the selected one: vehicle

--- vehicle.js
+++ vehicle.js
@@ -1,9 +1,18 @@
 var vehicles = document.getElementsByClassName("vehicle-button");
+var accNodes = document.getElementsByClassName("accordion");
 
 for (i = 0; i < vehicles.length; i++) {
   vehicles[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var elementId = this.id;
+    for(j=0; j < accNodes.length; j++) {
+      if(accNodes[j].id == 'vehicle') {
+        accNodes[j].getElementsByTagName("I")[0].innerHTML="home";
+        accNodes[j].getElementsByClassName("caption")[0].innerHTML="Captius";
+        accNodes[j].getElementsByClassName("description")[0].innerHTML="Desc";
+        accNodes[j].getElementsByClassName("price")[0].getElementsByTagName("SPAN")[0].innerHTML="Precin"
+      }
+    }
 
     for(j = 0; j< vehicles.length; j++) {
       if(vehicles[j].id != elementId) {  
@@ -12,3 +21,4 @@ for (i = 0; i < vehicles.length; i++) {
     }
   });
 }
+
