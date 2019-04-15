7e5632e8e6bb1f4af731fd27fea85c9eef8fa095
Ive <ive.tourinho@itmov.com.br>
2019-04-15 12:51:54 -0300

    See the selected one: payment

--- payment.js
+++ payment.js
@@ -1,24 +1,22 @@
-var vehicles = document.getElementsByClassName("vehicle-button");
+var payments = document.getElementsByClassName("payment-button");
 var accNodes = document.getElementsByClassName("accordion");
 
-for (i = 0; i < vehicles.length; i++) {
-  vehicles[i].addEventListener("click", function() {
+for (i = 0; i < payments.length; i++) {
+  payments[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var elementId = this.id;
     for(j=0; j < accNodes.length; j++) {
-      if(accNodes[j].id == 'vehicle') {
+      if(accNodes[j].id == 'payment') {
         accNodes[j].getElementsByTagName("I")[0].innerHTML = this.getElementsByTagName("I")[0].innerHTML;
         accNodes[j].getElementsByClassName("description")[0].innerHTML = this.getElementsByClassName("description")[0].innerHTML;
         accNodes[j].getElementsByClassName("caption")[0].innerHTML = this.getElementsByClassName("caption")[0].innerHTML;
-        accNodes[j].getElementsByClassName("price")[0].getElementsByTagName("SPAN")[0].innerHTML = this.getElementsByClassName("price")[0].getElementsByTagName("SPAN")[0].innerHTML;
       }
     }
 
-    for(j = 0; j< vehicles.length; j++) {
-      if(vehicles[j].id != elementId) {  
-        vehicles[j].classList.remove("active");
+    for(j = 0; j< payments.length; j++) {
+      if(payments[j].id != elementId) {  
+        payments[j].classList.remove("active");
       } 
     }
   });
 }
-
