dcc56cd88c738ebbd8c248fe8953bab1cc6c7a7c
Ive <ive.tourinho@itmov.com.br>
2019-04-16 10:59:18 -0300

    Close buttons

--- utility.js
+++ utility.js
@@ -4,14 +4,25 @@ for (i = 0; i < ubtns.length; i++) {
   ubtns[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var elementId = this.id;
-    closeOthers(elementId);
+    disableOthers(elementId);
   });
 }
 
-function closeOthers(elementId) {
+function disableOthers(elementId) {
   for (i = 0; i < ubtns.length; i++) {
     if(ubtns[i].id != elementId) {
       ubtns[i].classList.remove("active");
     }
   }
+}
+
+function closeParentPanel(evt, id) {
+  for(i = 0; i < acc.length; i++) {
+    if(acc[i].id === id) {
+      acc[i].classList.remove("active");
+    }
+  }
+  tab[0].style.display = 'flex';
+  submit[0].style.display = 'block';
+  evt.currentTarget.parentElement.parentElement.parentElement.style.maxHeight = null;
 }
