11455630b234f6fbbfa6a511bff02ce0b5c85ff2
Ive <ive.tourinho@itmov.com.br>
2019-04-16 12:43:33 -0300

    Change names: payment

--- payment.js
+++ payment.js
@@ -1,20 +1,20 @@
-var payments = document.getElementsByClassName("payment-button");
+var paymentss = document.getElementsByClassName("payments-button");
 
-for (i = 0; i < payments.length; i++) {
-  payments[i].addEventListener("click", function() {
+for (i = 0; i < paymentss.length; i++) {
+  paymentss[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var elementId = this.id;
     for(j=0; j < acc.length; j++) {
-      if(acc[j].id == 'payment') {
+      if(acc[j].id == 'payments') {
         acc[j].getElementsByTagName("I")[0].innerHTML = this.getElementsByTagName("I")[0].innerHTML;
         acc[j].getElementsByClassName("description")[0].innerHTML = this.getElementsByClassName("description")[0].innerHTML;
         acc[j].getElementsByClassName("caption")[0].innerHTML = this.getElementsByClassName("caption")[0].innerHTML;
       }
     }
 
-    for(j=0; j < payments.length; j++) {
-      if(payments[j].id !== elementId) {
-        payments[j].classList.remove("active");
+    for(j=0; j < paymentss.length; j++) {
+      if(paymentss[j].id !== elementId) {
+        paymentss[j].classList.remove("active");
       }
     }
 
